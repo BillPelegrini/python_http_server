a419582993ce0b48484a0d2428c003d4f263c78b
Billpelegrini <brunopelegrini@gmail.com>
2023-04-10 18:29:06 -0300

    adding http server script

---
+++ http_server.py
@@ -0,0 +1,19 @@
+#Para escutar uma conexão HTTP usando Python, você pode usar a biblioteca built-in http.server. 
+#Aqui está um exemplo de como criar um servidor HTTP simples que escuta na porta 8080 e responde com "Hello, World!" para qualquer solicitação recebida:
+
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class HelloHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(b'Hello, World!')
+
+httpd = HTTPServer(('', 8080), HelloHandler)
+httpd.serve_forever()
+
+#Este exemplo cria uma classe chamada HelloHandler que herda de BaseHTTPRequestHandler e sobrepõe o método do_GET para enviar uma resposta HTTP com o corpo "Hello, World!". 
+#Um objeto HTTPServer é então criado passando a classe HelloHandler como um argumento, e o método serve_forever é chamado para iniciar o loop de escuta do servidor.
+
+#Esse é um exemplo básico, você pode fazer muito mais coisas, como ler dados da requisição, enviar cabeçalhos específicos, usar frameworks para facilitar o desenvolvimento como Flask ou Django.
